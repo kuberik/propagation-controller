934ded56142ed2f6d5316575e79eb9b794a8d548
Luka Skugor <luka.skugor@chimeramail.com>
2023-09-24 13:28:52 +0200

    Get statuses API (#2)

--- pkg/status/status_test.go
+++ pkg/status/status_test.go
@@ -1,13 +1,10 @@
 package status
 
 import (
-	"bytes"
-	"encoding/json"
 	reflect "reflect"
 	"testing"
 	"time"
 
-	"github.com/google/go-containerregistry/pkg/crane"
 	v1 "github.com/google/go-containerregistry/pkg/v1"
 	"github.com/stretchr/testify/assert"
 	gomock "go.uber.org/mock/gomock"
@@ -30,12 +27,7 @@ func TestPublishStatuses(t *testing.T) {
 			return false
 		}
 
-		var extracted bytes.Buffer
-		err := crane.Export(image, &extracted)
-		assert.NoError(t, err)
-
-		got := []Status{}
-		err = json.Unmarshal(extracted.Bytes(), &got)
+		got, err := extractStatuses(image)
 		assert.NoError(t, err)
 
 		return reflect.DeepEqual(input, got)
@@ -46,4 +38,27 @@ func TestPublishStatuses(t *testing.T) {
 
 	err = client.PublishStatuses("staging", input)
 	assert.NoError(t, err)
+}
+
+func TestGetStatuses(t *testing.T) {
+	want := []Status{{
+		Version: "a",
+		Start:   time.Date(2023, 9, 24, 8, 42, 0, 0, time.UTC),
+	}, {
+		Version: "b",
+		Start:   time.Date(2023, 9, 24, 9, 42, 0, 0, time.UTC),
+	}}
+
+	ctrl := gomock.NewController(t)
+	ociClient := NewMockOCIClient(ctrl)
+	statusesImage, err := newStatusesImage(want)
+	assert.NoError(t, err)
+	ociClient.EXPECT().Pull("registry.example.local/deployments/foo:prod").Return(statusesImage, nil)
+
+	client, err := NewStatusClient("registry.example.local/deployments/foo", ociClient)
+	assert.NoError(t, err)
+
+	statuses, err := client.GetStatuses("prod")
+	assert.NoError(t, err)
+	assert.Equal(t, want, statuses)
 }
